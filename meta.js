781b8c8ae904c44816d838fc699b1f2aece7efad
Raj Singh <rrsingh@us.ibm.com>
2018-03-28 15:28:46 -0400

    fixed home sale name, version output

--- meta.js
+++ meta.js
@@ -1,7 +1,13 @@
+/**
+ * Reads a JSON file in the format conforming to the open data metadata guidelines 
+ * promulgated by the US Federal CIO office (https://project-open-data.cio.gov/v1.1/schema/)
+ * and writes an HTML file conforming to Google and schema.org
+ * https://developers.google.com/search/docs/data-types/dataset
+ * http://schema.org/Dataset
+ */
+
 var fs = require('fs')
 const metadata = require('./meta.json')
-
-const baseurl = 'http://s3-api.us-geo.objectstorage.softlayer.net/bucketname'
 
 htmlfile = '<!DOCTYPE html>\n<html lang="en">\n<head>\n'
 htmlfile += '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
@@ -13,6 +19,8 @@ htmlfile += '<h1>' + metadata.title + '</h1>\n'
 jsonld = ''
 htmlbody = ''
 metadata.datasets.forEach(dataset => {
+    htmlbody += '<div itemscope itemtype="http://schema.org/Dataset" itemid="' + dataset.identifier + '">\n'
+    htmlbody += '<meta itemprop="url" content="' + dataset.distribution[0].downloadURL + '"/>\n'
     jsonld += '<script type="application/ld+json">\n'
     jsonld += '{\n"@context": "http://schema.org",\n'
     jsonld += '"@type": "Dataset",\n'
@@ -22,17 +30,31 @@ metadata.datasets.forEach(dataset => {
     htmlbody += '<i>(' + dataset.identifier + ')</i>\n'
     jsonld += '"description": "' + dataset.description + '",\n'
     htmlbody += '<div class="description">' + dataset.description + '</div>\n'
+    if (dataset.publisher) {
+        htmlbody += '<div itemprop="publisher" itemscope itemtype="http://schema.org/Organization">\n'
+        htmlbody += 'Publisher: <span itemprop="name">' + dataset.publisher.name + '</span>\n'
+        htmlbody += '</div>\n'
+    }
+    jsonld += '"version": "' + dataset.modified + '",\n'
     jsonld += '"keywords": ["' + dataset.keyword.join('","') + '"],\n'
     htmlbody += '<div class="keywords">Keywords: ' + dataset.keyword.toString() + '</div>\n'
     jsonld += '"distribution": ['
     dataset.distribution.forEach(distro => {
         jsonld += '{\n"fileFormat": "' + distro.format + '",\n'
         jsonld += '"contentUrl": "' + distro.downloadURL + '"\n},\n'
-        htmlbody += '<div class="download-link">Download ' + distro.format + ': <a href="' + distro.downloadURL + '">' + distro.downloadURL + '</a></div>\n'
+        if (distro.sizeInBytes) {
+            mbsize = distro.sizeInBytes / 1000000
+            htmlbody += '<div itemprop="contentSize" content="' + mbsize.toString() + '">Size: ' + mbsize.toString() + ' MB</div>\n'
+        }
+        if (dataset.modified) {
+            htmlbody += '<div itemprop="version" content="' + dataset.modified + '">Last Modified: ' + dataset.modified + '</div>\n'
+        }
+        htmlbody += '<div itemprop="encodingFormat" content="' + distro.mediaType + '" class="download-link">Download ' + distro.format + ': <a href="' + distro.downloadURL + '">' + distro.downloadURL + '</a></div>\n'
     })
     jsonld = jsonld.substring(0, jsonld.length-2)
     jsonld += ']'
     jsonld += '}\n</script>\n'
+    htmlbody += "</div>\n"
 })
 htmlfile += jsonld
 htmlfile += htmlbody
